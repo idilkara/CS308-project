ec6a13ec58e679b7e8c08bcc41c63bc05cd6b4d0
idil <idilkara5@gmail.com>
2025-03-07 17:49:35 +0300

    containerize frontend

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import HomePage from "./HomePage"; 
-import CategoryPage from "./CategoryPage"; 
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+import CategoryPage from "./CategoryPage";
 
 function App() {
-  return <HomePage />; //directly opens home page
-
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/category" element={<CategoryPage />} />
+        <Route path="/" element={<HomePage />} /> {/* Default route */}
+      </Routes>
+    </Router>
+  );
 }
-
 
 export default App;
