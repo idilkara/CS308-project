8623070d3224aabd9c34e5b249788df23ae4678a
esinaydin <esin.aydin@sabanciuniv.edu>
2025-03-07 12:37:54 +0300

    added category page to frontend

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import HomePage from "./HomePage"; 
+import CategoryPage from "./CategoryPage"; 
+
 
 function App() {
-  return <HomePage />; //directly opens home page
+  return <CategoryPage />; //directly opens home page
+
 }
+
 
 export default App;
