c80a8ab804d92b833da77d458515489340b5e1f5
secilgezer <156457410+secilgezer@users.noreply.github.com>
2025-03-08 22:33:10 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
-from routes.user_routes import user_bp  # Import the user blueprint
-from routes.product_routes import products_bp  # Import the user blueprint
+from routes.user_routes import user_bp
+from routes.product_routes import products_bp
+from routes.auth_routes import auth_bp  # auth blueprint import et
+
 app = Flask(__name__)
 
-# Register Blueprints
+# JWT Secret Key (Auth Blueprint için)
+app.config["JWT_SECRET_KEY"] = "supersecretkey"
+
+# Blueprintleri Kaydet
 app.register_blueprint(user_bp, url_prefix="/users")
-app.register_blueprint(products_bp, url_prefix='/products')
+app.register_blueprint(products_bp, url_prefix="/products")
+app.register_blueprint(auth_bp, url_prefix="/auth")  # /auth ile çağıracağız
 
 @app.route("/")
 def hello_world():
