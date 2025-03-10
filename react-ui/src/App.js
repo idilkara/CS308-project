9520b427d50daa153ec2e6d13ec606cb7ed2c089
zeynepozgurgun <zeynepozgur@sabanciuniv.edu>
2025-03-10 22:14:48 +0300

    shopping cart added

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import CategoryPage from "./CategoryPage";
 import UserPage from "./UserPage";
+import ShoppingCart  from "./ShoppingCart";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/category" element={<CategoryPage />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        <Route path="/user" element={<UserPage />} />
         <Route path="/" element={<CategoryPage />} /> {/* Default route */}
       </Routes>
     </Router>
