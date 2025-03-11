b5dc88e733dd990715a53249e03b73d7cfaccda2
Duygu Gorgun <dgorgun@sabanciuniv.edu>
2025-03-11 18:12:10 +0300

    product view page

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -4,11 +4,8 @@ import HomePage from "./HomePage";
 import CategoryPage from "./CategoryPage";
 import UserPage from "./UserPage";
 import ShoppingCart  from "./ShoppingCart";
-<<<<<<< HEAD
 import OrdersPage from "./OrderPage";
-=======
-import ProductPage  from "./ProductPage";
->>>>>>> 00e637b4d3bbca18acc72aefed87f6222a1c3fd0
+import ProductPage from "./ProductPage";
 
 function App() {
   return (
@@ -18,13 +15,9 @@ function App() {
         <Route path="/category" element={<CategoryPage />} />
         <Route path="/cart" element={<ShoppingCart />} />
         <Route path="/user" element={<UserPage />} />
-<<<<<<< HEAD
         <Route path="/order" element={<OrdersPage />} />
+        <Route path="/product" element={<ProductPage />} />
         <Route path="/" element={<CategoryPage />} /> {/* Default route */}
-=======
-        <Route path="/" element={<ProductPage />} /> {/* Default route */}
-
->>>>>>> 00e637b4d3bbca18acc72aefed87f6222a1c3fd0
       </Routes>
     </Router>
   );
